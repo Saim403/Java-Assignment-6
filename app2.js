b4a65bff0537edba0669ce61312f9ab919bb757a
Saim403 <155624749+Saim403@users.noreply.github.com>
2024-02-09 15:54:32 +0500

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,12 @@
+ 
+ const myDiv = document.getElementById('myDiv');
+
+
+ myDiv.addEventListener('mouseenter', function() {
+   myDiv.style.backgroundColor = 'lightgreen';
+ });
+
+ 
+ myDiv.addEventListener('mouseleave', function() {
+   myDiv.style.backgroundColor = 'lightblue';
+ });
